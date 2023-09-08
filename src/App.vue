34f9f23c856c97f53ccc90204f1b35a8adc7cf3b
xuqiaoying1 <18402029763@163.com>
2023-09-08 21:51:31 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <first />
+</template>
+
+<script>
+import First from './components/first.vue'
+
+export default {
+  name: 'App',
+  components: {
+    First
+  }
+}
+</script>
---
+++ src/components/first.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>我的第一个组件</h1>
+    </div>
+</template>
