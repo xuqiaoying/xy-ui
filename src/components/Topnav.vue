acda217293d7ef9f1c17f29863fa5df7c9185de1
xuqiaoying1 <18402029763@163.com>
2023-09-12 20:08:50 +0800

    toggleAside样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
+        <span class="toggleAside"></span>
     </div>
 </template>
 
@@ -29,6 +30,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -39,6 +42,25 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  @media screen and (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
     }
   }
 }
